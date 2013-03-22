c6bed2c4b071e0841d0df880a6063f29cd06e641
Northside FIRST <ncp.first@gmail.com>
2013-03-22 10:46:57 -0500

    latest code on ClassmatePC

--- AxiomRobot/src/edu/wpi/first/wpilibj/templates/Axiom.java
+++ AxiomRobot/src/edu/wpi/first/wpilibj/templates/Axiom.java
@@ -20,15 +20,23 @@ public class Axiom extends SimpleRobot {
     /**
      * Configuration for motor controllers, joysticks, and other static variables.
      */
-
-    static final int REAR_LEFT_MOTOR_PWM = 4;
-    static final int REAR_RIGHT_MOTOR_PWM = 2;
-    static final int FRONT_LEFT_MOTOR_PWM = 3;
-    static final int FRONT_RIGHT_MOTOR_PWM = 1;
+    
+    //motor configuration
+    static final int FRONT_LEFT_MOTOR_PWM = 1;
+    static final int REAR_LEFT_MOTOR_PWM = 2;
+    static final int FRONT_RIGHT_MOTOR_PWM = 3;
+    static final int REAR_RIGHT_MOTOR_PWM = 4;
+    
+    //joystick configuration
     static final int LEFT_JOYSTICK_USB = 1;
     static final int RIGHT_JOYSTICK_USB = 2;
     
-    RobotDrive chassis = new RobotDrive(FRONT_LEFT_MOTOR_PWM, REAR_LEFT_MOTOR_PWM, FRONT_RIGHT_MOTOR_PWM, FRONT_LEFT_MOTOR_PWM);
+    Jaguar frontLeft = new Jaguar(FRONT_LEFT_MOTOR_PWM);
+    Victor rearLeft = new Victor(REAR_LEFT_MOTOR_PWM);
+    Jaguar frontRight = new Jaguar(FRONT_RIGHT_MOTOR_PWM);
+    Victor rearRight = new Victor(REAR_RIGHT_MOTOR_PWM);
+    
+    RobotDrive chassis = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
     Joystick leftJoystick = new Joystick(LEFT_JOYSTICK_USB);
     Joystick rightJoystick = new Joystick(RIGHT_JOYSTICK_USB);
     
@@ -36,10 +44,16 @@ public class Axiom extends SimpleRobot {
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        while (isAutonomous()) {
-            chassis.drive(0.5, 0.0); //move forward at 0.5 power
-            Timer.delay(2.0); //run for 2 seconds
-            chassis.drive(0.0, 0.0); //stop
+        /*
+        chassis.drive(0.2, 0.0); //move forward at 0.5 power
+        Timer.delay(2.0); //run for 2 seconds
+        chassis.drive(0.0, 0.0); //stop
+        */
+        while(isAutonomous()) {
+            frontLeft.set(0.2);
+            frontRight.set(-0.2);
+            rearLeft.set(0.2);
+            rearRight.set(-0.2);
         }
     }
 
@@ -49,8 +63,41 @@ public class Axiom extends SimpleRobot {
     public void operatorControl() {
         while (isOperatorControl()) {
             getWatchdog().feed();
-            double speed = leftJoystick.getThrottle();
-            chassis.arcadeDrive(speed*leftJoystick.getY(), speed*-leftJoystick.getX());
+            double speed = -((leftJoystick.getZ()-1)/2);
+            arcadeDrive(leftJoystick.getY(), leftJoystick.getX(), speed);
+            /*frontLeft.set(-leftJoystick.getY());
+            frontRight.set(rightJoystick.getY());
+            rearLeft.set(-leftJoystick.getY());
+            rearRight.set(rightJoystick.getY());*/
         }
+    }
+    
+    public void arcadeDrive(double moveValue, double rotateValue, double speedMult) {
+        // local variables to hold the computed PWM values for the motors
+        double leftMotorSpeed;
+        double rightMotorSpeed;
+        
+        if (moveValue > 0.0) {
+            if (rotateValue > 0.0) {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = Math.max(moveValue, rotateValue);
+            } else {
+                leftMotorSpeed = Math.max(moveValue, -rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            }
+        } else {
+            if (rotateValue > 0.0) {
+                leftMotorSpeed = -Math.max(-moveValue, rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            } else {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = -Math.max(-moveValue, -rotateValue);
+            }
+        }
+        
+        frontLeft.set(-leftMotorSpeed*speedMult);
+        frontRight.set(rightMotorSpeed*speedMult);
+        rearLeft.set(-leftMotorSpeed*speedMult);
+        rearRight.set(rightMotorSpeed*speedMult);
     }
 }
